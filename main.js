ef1039ec1e36516f64e81f3565659c3ddfb51a24
unknown <willianbcomiotto@gmail.com>
2023-01-27 20:54:24 -0300

    primeiro botão funcionando

--- main.js
+++ main.js
@@ -1,4 +1,18 @@
-const pergunta = document.querySelector('[data-pergunta]');
+const input = document.querySelector('[data-input]');
+const button = document.querySelector('[data-button]');
+const resposta = document.getElementById("text")
+
+
+
+button.onclick = function() {
+   
+    input.innerHTML = resposta.value + ", quantos anos você tem?"
+    
+}
+
+
+
+
         
     
 
