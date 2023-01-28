ee95b480eae80c3dcb3a13f6ea44b18099b06d2a
unknown <willianbcomiotto@gmail.com>
2023-01-28 14:02:34 -0300

    Primeira funcão rodando

--- main.js
+++ main.js
@@ -1,14 +1,46 @@
-const input = document.querySelector('[data-input]');
+const pergunta = document.querySelector('[data-pergunta]');
 const button = document.querySelector('[data-button]');
-const resposta = document.getElementById("text")
+const resposta = document.getElementById("text");
+const input = document.querySelector(".input");
 
+const pergunta2 =  resposta.value + "Quantos anos você tem?"
+const pergunta3 = "Qual linguagem de programação você está estudando?"
 
+const listaPerguntas = [pergunta2, pergunta3]
 
-button.onclick = function() {
-   
-    input.innerHTML = resposta.value + ", quantos anos você tem?"
+input.focus();
+
+let contador = 0;
+
+function mudaPerguntas() {
     
+    if(contador < listaPerguntas.length) {
+        pergunta.textContent = `${listaPerguntas[contador]}`
+    }
+
+    contador++;
+
 }
+
+button.addEventListener('click', () => {
+    
+    mudaPerguntas();
+
+    input.focus();
+    input.value = "";
+
+})
+
+
+
+
+
+
+
+
+
+
+
 
 
 
